1150ca69412244a5da1b55da7af413d24ee7a853
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-14 19:50:52 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    printf("                               ", "\n");
+    printf("    BEM VINDO AO MEU MUNDO!    ", "\n");
+    printf("                               ", "\n");
+
+}
