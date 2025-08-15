3df412446654c4c472926e2d0beb165600e018f1
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-14 22:12:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+void tela_inicial(void);
+void tela_sobre(void);
+void tela_equipe(void);
 
 int main(void){
-
-    printf("                               ", "\n");
-    printf("    BEM VINDO AO MEU MUNDO!    ", "\n");
-    printf("                               ", "\n");
+    setlocale(LC_ALL,"Portuguese");
+    tela_inicial();
 
     return(0);
+}
+
+void tela_inicial(void){
+    system("clear||cls");
+    char op;
+    printf("\n");
+    printf("____________________________________________________________________");
+    printf("|                                                                  |");
+    printf("|             SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS             |");
+    printf("|                                                                  |");
+    printf("|            1- Cliente                                            |");
+    printf("|            2- Quartos                                            |");
+    printf("|            3- Serviços                                           |");
+    printf("|            4- Relatório                                          |");
+    printf("|            0- Sair                                               |");
+    printf("|                                                                  |");
+    printf("|   Escolha a opção:                                               |");
+    scanf("%d", &op);
+    getchar();
+    printf("|                                                                  |");
+    printf("____________________________________________________________________");
+    printf("\t\t PRESSIONE ENTER PARA CONTINUAR...");
+
 }
