3debcd59739dd846bad4d80415565ed20d2b413c
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-16 20:32:13 -0300

    Update main.c
    teste.

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 int main(void){
     setlocale(LC_ALL,"Portuguese");
 
+    printf("BEM VINDO AO MEU MUNDO!");
+    
+    /*      BEM VINDO
+        VEJA MEU SISTEMA
+        SEJA BEM VINDO(A)  */
 
     return(0);
 }
