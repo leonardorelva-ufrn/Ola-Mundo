f70e43680181c7091aa2abd3f3372466c845ff7a
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-16 20:20:20 -0300

    Retirando a função

--- main.c
+++ main.c
@@ -12,26 +12,3 @@ int main(void){
 
     return(0);
 }
-
-void tela_inicial(void){
-    system("clear||cls");
-    char op;
-    printf("\n");
-    printf("____________________________________________________________________");
-    printf("|                                                                  |");
-    printf("|             SISTEMA DE GESTÃO PARA HOTÉIS E POUSADAS             |");
-    printf("|                                                                  |");
-    printf("|            1- Cliente                                            |");
-    printf("|            2- Hospedagem                                         |");
-    printf("|            3- Alimentação                                        |");
-    printf("|            4- Relatório                                          |");
-    printf("|            0- Sair                                               |");
-    printf("|                                                                  |");
-    printf("|   Escolha a opção:                                               |");
-    scanf("%d", &op);
-    getchar();
-    printf("|                                                                  |");
-    printf("____________________________________________________________________");
-    printf("\t\t PRESSIONE ENTER PARA CONTINUAR...");
-
-}
