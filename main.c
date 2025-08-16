9dee82805ceb81d91e25b681baff592d55ad12f1
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-08-16 20:28:21 -0300

    Update main.c
    retirando as funçoes.

--- main.c
+++ main.c
@@ -2,13 +2,10 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void tela_inicial(void);
-void tela_sobre(void);
-void tela_equipe(void);
 
 int main(void){
     setlocale(LC_ALL,"Portuguese");
-    tela_inicial();
+
 
     return(0);
 }
